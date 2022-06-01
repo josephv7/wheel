581cb04e5afdd546c750dd04a60c5800ae8bd124
Joseph Varghese <josephvarghese.rms@gmail.com>
2022-06-01 09:03:04 +0530

    Added id key to lists stored in constants

--- app/javascript/src/components/Dashboard/Notes/constants.js
+++ app/javascript/src/components/Dashboard/Notes/constants.js
@@ -20,31 +20,37 @@ export const TAGS = [
     label: "Getting Started",
     value: "getting_started",
     count: 80,
+    id: 1,
   },
   {
     label: "Onboarding",
     value: "onboarding",
     count: 20,
+    id: 2,
   },
   {
     label: "User Flow",
     value: "user_flow",
     count: 10,
+    id: 3,
   },
   {
     label: "UX",
     value: "ux",
     count: 10,
+    id: 4,
   },
   {
     label: "Bugs",
     value: "bugs",
     count: 10,
+    id: 5,
   },
   {
     label: "V2",
     value: "V2",
     count: 10,
+    id: 6,
   },
 ];
 
@@ -53,21 +59,25 @@ export const TYPES = [
     label: "All",
     value: "all",
     count: 90,
+    id: 1,
   },
   {
     label: "Users",
     value: "users",
     count: 80,
+    id: 2,
   },
   {
     label: "Leads",
     value: "leads",
     count: 20,
+    id: 3,
   },
   {
     label: "Visitors",
     value: "visitors",
     count: 10,
+    id: 4,
   },
 ];
 
@@ -76,16 +86,19 @@ export const SEGMENTS = [
     label: "Europe",
     value: "europe",
     count: 90,
+    id: 1,
   },
   {
     label: "Middle-East",
     value: "middle_east",
     count: 20,
+    id: 2,
   },
   {
     label: "Asia",
     value: "asia",
     count: 80,
+    id: 3,
   },
 ];
 
@@ -94,11 +107,13 @@ export const CONTACTS = [
     label: "Oliver Smith",
     value: "Oliver Smith",
     email: "oliver@example.com",
+    id: 1,
   },
   {
     label: "John Doe",
     value: "John Doe",
     email: "john@example.com",
+    id: 2,
   },
 ];
 
