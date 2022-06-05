e297646c052219f684910ebad26d1f90c11269fe
Joseph Varghese <josephvarghese.rms@gmail.com>
2022-06-05 23:41:47 +0530

    Added logic to render avatar with namme in contacts table

--- app/javascript/src/components/Dashboard/Contacts/Table.jsx
+++ app/javascript/src/components/Dashboard/Contacts/Table.jsx
@@ -1,35 +1,48 @@
 import React, { useState } from "react";
 
 import { MenuHorizontal } from "neetoicons";
-import { Table as NeetoUITable, Dropdown } from "neetoui";
+import { Table as NeetoUITable, Dropdown, Avatar } from "neetoui";
 
 import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE, CONTACTS } from "./constants";
 import { renderColumnData } from "./utils";
 
 const Table = ({ setIsDeleteAlertOpen, contacts = [] }) => {
   const [pageNo, setPageNo] = useState(DEFAULT_PAGE_INDEX);
+  let columnData = renderColumnData();
 
-  const columnData = renderColumnData();
-
-  columnData.push({
-    title: "",
-    dataIndex: "actions",
-    key: "actions",
-    render: () => (
-      <div onClick={e => e.stopPropagation()}>
-        <Dropdown
-          buttonStyle="text"
-          buttonProps={{ size: "large" }}
-          position="bottom-end"
-          icon={MenuHorizontal}
-          strategy="fixed"
-        >
-          <li>Edit</li>
-          <li onClick={() => setIsDeleteAlertOpen(true)}>Delete</li>
-        </Dropdown>
-      </div>
-    ),
-  });
+  columnData = [
+    {
+      title: "Name & Role",
+      dataIndex: "name",
+      key: "name",
+      render: name => (
+        <div className="flex flex-row items-center">
+          <Avatar user={{ name: name }} size="small" className="mr-2" />
+          {name}
+        </div>
+      ),
+    },
+    ...columnData.slice(1),
+    {
+      title: "",
+      dataIndex: "actions",
+      key: "actions",
+      render: () => (
+        <div onClick={e => e.stopPropagation()}>
+          <Dropdown
+            buttonStyle="text"
+            buttonProps={{ size: "large" }}
+            position="bottom-end"
+            icon={MenuHorizontal}
+            strategy="fixed"
+          >
+            <li>Edit</li>
+            <li onClick={() => setIsDeleteAlertOpen(true)}>Delete</li>
+          </Dropdown>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <NeetoUITable
